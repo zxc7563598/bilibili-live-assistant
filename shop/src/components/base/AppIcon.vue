acd43670860d3f9e0ac9bfa9b02b3904aa90306f
zxc7563598 <junjie.he.925@gmail.com>
2026-08-26 15:48:29 +0800

    feat(shop): 补充 AppIcon 邮箱、编辑、删除图标

--- shop/src/components/base/AppIcon.vue
+++ shop/src/components/base/AppIcon.vue
@@ -41,6 +41,9 @@ const icons = {
   'share': '<path d="M4 12v7a1 1 0 0 0 1 1h14a1 1 0 0 0 1-1v-7"/><path d="m16 6-4-4-4 4"/><path d="M12 2v13"/>',
   'plus': '<path d="M12 5v14"/><path d="M5 12h14"/>',
   'minus': '<path d="M5 12h14"/>',
+  'mail': '<rect x="3" y="5" width="18" height="14" rx="2"/><path d="m3 7 9 6 9-6"/>',
+  'edit': '<path d="M12 20h9"/><path d="M16.5 3.5a2.1 2.1 0 0 1 3 3L7 19l-4 1 1-4z"/>',
+  'trash': '<path d="M3 6h18"/><path d="M8 6V4a1 1 0 0 1 1-1h6a1 1 0 0 1 1 1v2"/><path d="m6 6 1 14a2 2 0 0 0 2 2h6a2 2 0 0 0 2-2l1-14"/><path d="M10 11v6"/><path d="M14 11v6"/>',
 }
 
 const markup = computed(() => icons[props.name] || icons.image)
